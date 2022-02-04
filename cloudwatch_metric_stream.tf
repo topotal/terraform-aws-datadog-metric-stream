20960ced484e089efd103d20e89a4c69684c9025
Ryota Yoshikawa <yoshikawa@topotal.com>
2022-02-04 17:14:49 +0900

    Add include_filter with iterator to support Metric filter

--- cloudwatch_metric_stream.tf
+++ cloudwatch_metric_stream.tf
@@ -3,4 +3,13 @@ resource "aws_cloudwatch_metric_stream" "datadog" {
   firehose_arn  = aws_kinesis_firehose_delivery_stream.datadog.arn
   output_format = var.cloudwatch_metric_stream_output_format
   role_arn      = aws_iam_role.datadog-metric-stream.arn
+
+  dynamic "include_filter" {
+    for_each = var.metric_stream_namespace_list
+    iterator = namespace
+
+    content {
+      namespace = namespace.value
+    }
+  }
 }
